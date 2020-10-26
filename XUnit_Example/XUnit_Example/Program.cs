a05e351607b498e0130a7602ad0d057309eadbf8
dhmec <dharmoradiya@gmail.com>
2020-10-26 12:57:40 -0600

    Add Few more mothods for test

--- XUnit_Example/XUnit_Example/Program.cs
+++ XUnit_Example/XUnit_Example/Program.cs
@@ -12,5 +12,18 @@ namespace XUnit_Example
         {
             return number / 2;
         }
+        // In-class practice:
+        // Write the method to determine if a number is a perfect square. Should return true if so, and false if not.
+        // Write tests for the edge cases and any other significant values.
+        // If you can't get the method working after 5 minutes, write the tests and come back.
+        public static bool PerfectSquare(int number)
+        {
+            return Math.Sqrt(number) == Math.Ceiling(Math.Sqrt(number));
+        }
+
+        public static void ThrowsFormatException()
+        {
+            int.Parse("Hello");
+        }
     }
 }
