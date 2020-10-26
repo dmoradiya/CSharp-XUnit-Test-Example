ab853b155633775b278e1ab84527c3a0e3f4fc64
dhmec <dharmoradiya@gmail.com>
2020-10-26 12:52:32 -0600

    Add Xunit Test for the method DividedByTwo

--- XUnit_Example/XUnit_Example/Program.cs
+++ XUnit_Example/XUnit_Example/Program.cs
@@ -2,11 +2,15 @@
 
 namespace XUnit_Example
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+        public static int DivideByTwo(int number)
+        {
+            return number / 2;
         }
     }
 }
--- XUnit_Example/XUnit_Example_Test/UnitTest1.cs
+++ XUnit_Example/XUnit_Example_Test/UnitTest1.cs
@@ -1,14 +1,31 @@
 using System;
 using Xunit;
+using XUnit_Example;
 
 namespace XUnit_Example_Test
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        // A theory is meant for testing a method with a set of data. Typically you won't be able to test all permutations or possible inputs of the method (example being math operations), however it is good to test edge cases as well as areas around zero.
+        // If you method works for all ints, check the max and min values, zero, -1 and 1, for example.
+        [Theory]
+        [InlineData(2, 1)]
+        [InlineData(4, 2)]
+        [InlineData(3, 1)]
+        [InlineData(0, 0)]
+        [InlineData(-2, -1)]
+        [InlineData(int.MaxValue, 1073741823)]
+        [InlineData(int.MinValue, -1073741824)]
+        public void DivideByTwo_Valid(int input, int expected)
         {
+            // Arrange - Set up any data, variables, objects, etc necessary for testing.
+            int output;
 
+            // Act - Call the method, setter, whatever you're testing.
+            output = Program.DivideByTwo(input);
+
+            // Assert - Check the result to make sure it matches what's expected.
+            Assert.Equal(expected, output);
         }
     }
 }
