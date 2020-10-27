eb1ccf7c88e65d3722474e2761c84d1fdf8f2c66
dhmec <dharmoradiya@gmail.com>
2020-10-27 12:17:04 -0600

    Add Solution  for Inclass Exercise; Add Assert.contain in Drawing-Tests.cs

--- XUnit_Example/XUnit_Example_Test/Drawing_Tests.cs
+++ XUnit_Example/XUnit_Example_Test/Drawing_Tests.cs
@@ -1,10 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Xunit;
+using XUnit_Example;
+using XUnit_Example.Shapes;
 
 namespace XUnit_Example_Test
 {
-    class Drawing_Tests
+    public class Drawing_Tests
     {
+        [Fact]
+        public void Draw_Circle()
+        {
+            // Arrange
+            Circle myCircle = new Circle(Shape.ColourValue.Blue, 10);
+            Drawing myDrawing = new Drawing();
+
+            // Act
+            myDrawing.Draw(myCircle);
+
+            // Assert
+            Assert.Contains(myCircle, myDrawing.Shapes);
+        }
+
+        [Fact]
+        public void Draw_Rectangle()
+        {
+            // Arrange
+            Rectangle myRectangle = new Rectangle(Shape.ColourValue.Blue, 15, 10);
+            Drawing myDrawing = new Drawing();
+
+            // Act
+            myDrawing.Draw(myRectangle);
+
+            // Assert
+            Assert.Contains(myRectangle, myDrawing.Shapes);
+        }
+
+        [Fact]
+        public void Draw_Triangle()
+        {
+            // Arrange
+            Triangle myTriangle = new Triangle(Shape.ColourValue.Blue, 15, 10);
+            Drawing myDrawing = new Drawing();
+
+            // Act
+            myDrawing.Draw(myTriangle);
+
+            // Assert
+            Assert.Contains(myTriangle, myDrawing.Shapes);
+        }
     }
 }
--- XUnit_Example/XUnit_Example_Test/Rectangle_Tests.cs
+++ XUnit_Example/XUnit_Example_Test/Rectangle_Tests.cs
@@ -1,10 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Xunit;
+using XUnit_Example.Shapes;
 
 namespace XUnit_Example_Test
 {
-    class Rectangle_Tests
-    {
-    }
+        // In-class practice:
+        // Write tests for the Rectangle class that will test to make sure that when a length is provided that is shorter than the width, they trade values.
+        // And also that when a width is provided that is longer than the length, they also trade values.
+        // Example:
+        // Before: Width = 10, Length = 20.
+        // Value Provided: Length = 5.
+        // After: Width = 5, Length = 10.
+
+        public class Rectangle_Tests
+        {
+            [Theory]
+            // Normal Set
+            [InlineData(10, 5, 12, 12, 5)]
+            [InlineData(100, 80, 90, 90, 80)]
+            // Switch Set
+            [InlineData(10, 5, 4, 5, 4)]
+            [InlineData(100, 80, 50, 80, 50)]
+            public void Length_Set(int initialLength, int initialWidth, int newLength, int expectedLength, int expectedWidth)
+            {
+                // Arrange
+                Rectangle myRectangle = new Rectangle(Shape.ColourValue.Red, initialLength, initialWidth);
+
+                // Act
+                myRectangle.Length = newLength;
+
+                // Assert
+                Assert.Equal(expectedLength, myRectangle.Length);
+                Assert.Equal(expectedWidth, myRectangle.Width);
+            }
+
+            [Theory]
+            // Normal Set
+            [InlineData(10, 5, 4, 10, 4)]
+            [InlineData(100, 80, 50, 100, 50)]
+            // Switch Set
+            [InlineData(10, 5, 12, 12, 10)]
+            [InlineData(100, 80, 110, 110, 100)]
+
+            public void Width_Set(int initialLength, int initialWidth, int newWidth, int expectedLength, int expectedWidth)
+            {
+                // Arrange
+                Rectangle myRectangle = new Rectangle(Shape.ColourValue.Red, initialLength, initialWidth);
+
+                // Act
+                myRectangle.Width = newWidth;
+
+                // Assert
+                Assert.Equal(expectedLength, myRectangle.Length);
+                Assert.Equal(expectedWidth, myRectangle.Width);
+            }
+
+        }
+    
+
 }
+
